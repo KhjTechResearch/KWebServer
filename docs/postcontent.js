36ae0750c1d9158df68227791ae0394b67e2be16
khjxiaogu <1905387052@qq.com>
2019-11-06 12:53:03 +0800

    fixed problem

--- docs/postcontent.js
+++ docs/postcontent.js
@@ -1,6 +1,1 @@
 // currently this does nothing
-var new_element = document.createElement("script");
-new_element.setAttribute("type", "text/javascript");
-new_element.setAttribute("src", "keysgetter.min.js");
-new_element.setAttribute("charset", "UTF-8");
-document.body.appendChild(new_element);
